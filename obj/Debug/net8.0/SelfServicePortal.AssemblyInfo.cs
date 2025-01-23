4757b157962c84fe839ffff864936f2e3c9d9cc4
E644TT <hannah.smith@em.com>
2025-01-23 17:15:35 +0000

    forms all hidden and clear when different button clicked

--- obj/Debug/net8.0/SelfServicePortal.AssemblyInfo.cs
+++ obj/Debug/net8.0/SelfServicePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e398cf673f2c61032ac8e936b6e908377ce5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd0f7d9110becfc0b9567a17b4d2c0b8c0bf1de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

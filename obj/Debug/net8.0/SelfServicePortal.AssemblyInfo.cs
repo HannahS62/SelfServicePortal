6bfcc8f1b2d8dee6ef77be3c5a190d7d5a204828
E644TT <hannah.smith@em.com>
2025-01-27 17:00:44 +0000

    forms and functions now working, css upadated

--- obj/Debug/net8.0/SelfServicePortal.AssemblyInfo.cs
+++ obj/Debug/net8.0/SelfServicePortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4757b157962c84fe839ffff864936f2e3c9d9cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e53b97f24b1c6a62b9b45e0dcafdb8ce12b3a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelfServicePortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
